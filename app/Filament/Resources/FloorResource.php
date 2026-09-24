9d5699f08ba7fb9450c0a0436a36f29360d65ac4
Piyush Shailendra Kumar Raikwar <piyushraikwar289@gmail.com>
2026-09-24 13:42:45 +0530

    Add authorization methods to FloorResource for role-based access control

--- app/Filament/Resources/FloorResource.php
+++ app/Filament/Resources/FloorResource.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Support\Facades\Auth;
 use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
+use Filament\Facades\Filament;
 
 class FloorResource extends Resource
 {
@@ -21,7 +22,28 @@ class FloorResource extends Resource
     protected static ?string $navigationGroup = 'Branches';
     protected static ?string $label = 'Floor';
     protected static ?string $pluralLabel = 'Floors';
-    protected static ?int $navigationSort = 6;
+    protected static ?int $navigationSort = 6;   
+
+    
+public static function canViewAny(): bool
+{
+    return Filament::auth()->user()?->can('view_any_floor') ?? false;
+}
+
+public static function canCreate(): bool
+{
+    return Filament::auth()->user()?->can('create_floor') ?? false;
+}
+
+public static function canEdit($record): bool
+{
+    return Filament::auth()->user()?->can('update_floor') ?? false;
+}
+
+public static function canDelete($record): bool
+{
+    return Filament::auth()->user()?->can('delete_floor') ?? false;
+}
 
     public static function form(Form $form): Form
     {
